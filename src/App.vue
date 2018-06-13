9992ea0a4ce3108e476a05b77a4debff66ad0fc7
Anders Jepsen <anders@magenta.dk>
2018-06-13 10:08:28 +0200

    fixed login

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,25 @@
 
 <script>
 import BfHeader from '@/components/BfHeader'
+import { HTTP } from '@/api/Http'
+import { AUTH_LOGOUT } from '@/vuex/actions/auth'
+
 export default {
   name: 'App',
   components: {
     BfHeader
+  },
+  created () {
+    HTTP.interceptors.response.use(undefined, err => {
+      return new Promise(function (resolve, reject) {
+        if (err.response.status === 401 && err.response.config && !err.response.config.__isRetryRequest) {
+          this.$store.dispatch(AUTH_LOGOUT)
+          this.$router.push('/')
+          this.$router.go()
+        }
+        throw err
+      })
+    })
   }
 }
 </script>
